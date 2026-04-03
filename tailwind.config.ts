6027f4644224b1cb2d9d8a8871c8e5ea416f3ca9
Ob17oo <liemil534@gmail.com>
2026-04-03 04:39:54 +0300

    AnotherFixOfTailwind&PostCss

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,10 @@
 import type { Config } from 'tailwindcss';
 
 export default {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
     extend: {},
   },
